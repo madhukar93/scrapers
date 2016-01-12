7bd4bf314e63461ec7658da54e0bb85350633068
Madhukar Mishra <madhukar.mishra@grofers.com>
2016-01-12 18:17:07 +0530

    I'm done yo.

--- ptap.py
+++ ptap.py
@@ -12,12 +12,30 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 fh = logging.FileHandler('ptap_errors.log')
 fh.setFormatter(formatter)
 logger.addHandler(fh)
+logger.error("===========reloaded==============")
 
 
 class PepperTap:
     base_url = "https://api.peppertap.com"
     # should be imported from database connection file
     db_conn = sqlite3.connect('competitor_inventory_test1.db')
+
+    def clear_table_data(self):
+        self.db_cursor.execute(
+            """
+            DELETE FROM peppertap_product
+            """
+        )
+        self.db_cursor.execute(
+            """
+            DELETE FROM peppertap_zone
+            """
+        )
+        self.db_cursor.execute(
+            """
+            DELETE FROM peppertap_zone_product_map
+            """
+        )
 
     def _get_cities(self):
         cities_url = "/location/v1/cities/"
@@ -42,42 +60,55 @@ class PepperTap:
         zones = {}
         for area in self.areas:
             try:
-                zones[area['zone_id']]['city'] = area['city'],
-                zones['areas'].append(area['area'])
+                zones[area['zone_id']]['city'] = area['city']
+                zones[area['zone_id']]['areas'].append(area['area'])
             except KeyError:
                 zones[area['zone_id']] = {
                     'city': area['city'],
-                    'areas' = [area['area']]
+                    'areas': [area['area']]
                 }
+        self.zones = zones
         for zone_id, values in zones.iteritems():
-            self._insert_zone(
-                (
-                    zone_id,
-                    values['area'],
-                    values['city'],
+            try:
+                self._insert_zone(
+                    (
+                        zone_id,
+                        ', '.join(values['areas']),
+                        values['city']
+                    )
                 )
-            )
+            except Exception as inst:
+                logger.error(
+                    "{type}: {message}: {data}\n\t=====\n".format(
+                        type=type(inst),
+                        message=str(inst),
+                        data=': '.join([str(zone_id), str(values)])
+                    )
+                )
 
     def _insert_zone(self, values):
-        insert_query = "insert into peppertap_zone (?, ?, ?, ?)"
+        insert_query = "INSERT INTO peppertap_zone VALUES (?, ?, ?)"
         self.db_cursor.execute(
             insert_query,
             values
         )
+        PepperTap.db_conn.commit()
 
     def _insert_product(self, values):
-        insert_query = "insert into peppertap_product (?, ?, ?, ?, ?, ?, ?)"
+        insert_query = "INSERT INTO peppertap_product VALUES (?, ?, ?, ?, ?, ?, ?)"
         self.db_cursor.execute(
             insert_query,
             values
         )
+        PepperTap.db_conn.commit()
 
     def _insert_product_zone_mapping(self, values):
-        insert_query = "insert into peppertap_zone_product_map (?, ?)"
+        insert_query = "INSERT INTO peppertap_zone_product_map VALUES (?, ?)"
         self.db_cursor.execute(
             insert_query,
             values
         )
+        PepperTap.db_conn.commit()
 
     def _get_categories(self, zone_id):
         categories_url = "/user/shop/categories/"
@@ -88,40 +119,77 @@ class PepperTap:
         )
         return response.json()['categories']
 
-    def __init__(self, args, kwargs):
+    def __init__(self, *args, **kwargs):
         try:
             import setup_db_ptap
-        except:
-            pass
-        self._get_zones()
+        except Exception as inst:
+            logger.error(
+                "{type}: {message}\n\t=====\n".format(
+                    type=type(inst),
+                    message=str(inst),
+                )
+            )
         self.db_cursor = PepperTap.db_conn.cursor()
+        self._get_areas()
+        self._write_zones()
 
-    def get_products_by_category(self, category_dict, zone_dict):
+    def get_products_by_category(self, category, zone_id):
         products_url = "/user/shop/{cid}/products/"
-        params = {'zone_id': zone['zone_id']}
+        params = {'zone_id': zone_id}
         response = requests.get(
             PepperTap.base_url + products_url.format(
-                cid=category_id
+                cid=category['id']
             ),
-            params=params
+            params=params,
+            verify=False
         )
         products = response.json()['pl']
         for product in products:
-            insert_product(
-                (
-                    product['ps']['uid'],
-                    product['ps']['sp'],
-                    product['ps']['mrp'],
-                    product['tle'],
-                    ", ".join(product['typ']),
-                    category['name'],
-                    product['ps']['da'],
+            try:
+                self._insert_product(
+                    (
+                        product['ps'][0]['uid'],
+                        product['ps'][0]['sp'],
+                        product['ps'][0]['mrp'],
+                        product['tle'],
+                        ", ".join(product['typ']),
+                        category['name'],
+                        product['ps'][0]['da'],
+                    )
                 )
-            )
+            except Exception as inst:
+                logger.error(
+                    "{type}: {message}: {data}\n\t=====\n".format(
+                        type=type(inst),
+                        message=str(inst),
+                        data=str(product)
+                    )
+                )
+            try:
+                self._insert_product_zone_mapping(
+                    (
+                        zone_id,
+                        product['ps'][0]['uid'],
+                    )
+                )
+            except Exception as inst:
+                logger.error(
+                    "{type}: {message}: {data}\n\t=====\n".format(
+                        type=type(inst),
+                        message=str(inst),
+                        data="product uid {}, zone_id {}".format(
+                            product['ps'][0]['uid'], zone_id
+                        )
+                    )
+                )
 
     def get_products_by_zone(self, zone_id):
-        categories = self._get_categories_by_zone(zone_id)
+        categories = self._get_categories(zone_id)
         for category in categories:
-            for child in category['children']:
+            for child_category in category['children']:
                 # should be pushing this to the task queue
-                pass
+                self.get_products_by_category(child_category, zone_id)
+
+    def get_all_products(self):
+        for zone_id in self.zones.keys():
+            get_products_by_zone(zone_id)
--- setup_db_ptap.py
+++ setup_db_ptap.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+from ptap import logger
 conn = sqlite3.connect('competitor_inventory_test1.db')
 c = conn.cursor()
 
@@ -24,41 +24,64 @@ product object
   "tle": "Britannia 50-50 Maska Chaska" title
 }
 """
+try:
+    c.execute(
+        '''CREATE TABLE peppertap_product
+           (
+                uid text PRIMARY KEY,
+                sp real,
+                mrp real,
+                title text,
+                subcats text,
+                category text,
+                unit text
+            )
+        '''
+    )
+except sqlite3.OperationalError as inst:
+    logger.error(
+                    "{type}: {message}\n\t=====\n".format(
+                        type=type(inst),
+                        message=str(inst),
+                    )
+                )
 
-c.execute(
-    '''CREATE TABLE peppertap_product
-       (
-            uid text PRIMARY KEY,
-            sp real,
-            mrp real,
-            title text,
-            subcats text,
-            category text,
-            unit text,
-        )
-    '''
-)
+try:
+    c.execute(
+        '''CREATE TABLE peppertap_zone
+           (
+                zone_id int PRIMARY KEY,
+                areas text,
+                city text
+            )
+        '''
+    )
+except sqlite3.OperationalError as inst:
+    logger.error(
+                    "{type}: {message}\n\t=====\n".format(
+                        type=type(inst),
+                        message=str(inst),
+                    )
+                )
 
-c.execute(
-    '''CREATE TABLE peppertap_zone
-       (
-            zone_id int PRIMARY KEY,
-            areas text,
-            city text
-        )
-    '''
-)
-
-c.execute(
-    '''CREATE TABLE peppertap_zone_product_map
-       (
-            zone_id int,
-            uid text,
-            PRIMARY KEY (zone_id, uid),
-            FOREIGN KEY(zone_id)
-                REFERENCES peppertap_zone(zone_id),
-            FOREIGN KEY(uid)
-                REFERENCES peppertap_product(uid),
-        )
-    '''
-)
+try:
+    c.execute(
+        '''CREATE TABLE peppertap_zone_product_map
+           (
+                zone_id int,
+                uid text,
+                PRIMARY KEY (zone_id, uid),
+                FOREIGN KEY(zone_id)
+                    REFERENCES peppertap_zone(zone_id),
+                FOREIGN KEY(uid)
+                    REFERENCES peppertap_product(uid)
+            )
+        '''
+    )
+except sqlite3.OperationalError as inst:
+    logger.error(
+                    "{type}: {message}\n\t=====\n".format(
+                        type=type(inst),
+                        message=str(inst),
+                    )
+                )
